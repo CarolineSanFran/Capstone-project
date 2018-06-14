ed0b4c832e981e69d902727209b855c26a57f3e2
Caroline SanFran <c@sanfranciscoconsult.com>
2018-06-14 12:00:58 -0400

    June14_Project

--- Twitter_API_Back/server.js
+++ Twitter_API_Back/server.js
@@ -14,9 +14,12 @@ require('dotenv').config();
 app.listen(8080, () => {
   console.log('Server Started on http://localhost:8080');
   console.log('Press CTRL + C to stop server');
+  
 });
 
 let tweetsForApp = [];
+
+
 
 let blockedUser = [];
 
